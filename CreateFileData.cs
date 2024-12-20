5576627887adb43f711d0ec49ef241ead318a03b
DimitryRezvyakov <dimrez199@gmail.com>
2024-12-20 23:51:32 +0300

    Add files via upload

---
+++ CreateFileData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debtors.GetData
+{
+    internal class CreateFileData : ICreateFileData
+    {
+        public IFileData MCreateFileData()
+        {
+
+                string name = Console.ReadLine();
+                DateTime creationDate;
+
+                if (!DateTime.TryParse(Console.ReadLine(), out creationDate))
+                    throw new FormatException("Неверный формат");
+
+                string path = Console.ReadLine();
+                string fileName = Console.ReadLine();
+
+                if (new string[] { name, path, fileName }.All(elem => !string.IsNullOrEmpty(elem)))
+                    return new FileData(name, creationDate, path, fileName);
+
+                throw new FormatException("Неверные значения");
+        }
+    }
+}
