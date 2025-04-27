c30ac465f4ece830ab5c318df740d9e4758a2535
Dimitry Rezvyackov <dimrez199@gmail.com>
2025-04-27 16:25:14 +0300

    AADT HomeWork Boier-Moore Alghorithm

---
+++ BoierMourAlghorithm/BoierMourAlghorithm/BoyerMoore.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BierMourAlghorithm
+{
+
+
+        public class BoyerMoore
+        {
+            public int OperationsCount { get; private set; }
+
+            public List<int> Search(string text, string pattern)
+            {
+                int n = pattern.Length;
+                int m = text.Length;
+                var positions = new List<int>();
+
+                int[] bpos = new int[n + 1];
+                int[] goodSuffixShift = new int[n + 1];
+                var badCharTable = BuildBadCharTable(pattern);
+
+                PreprocessStrongSuffix(goodSuffixShift, bpos, pattern, n);
+                PreprocessCase2(goodSuffixShift, bpos, pattern, n);
+
+                int s = 0;
+                while (s <= m - n)
+                {
+                    int j = n - 1;
+
+                    while (j >= 0 && pattern[j] == text[s + j])
+                    {
+                        OperationsCount++;   // <----- для отладки
+                        j--;
+                    }
+
+                    OperationsCount += j >= 0 ? 1 : 0;
+
+                    if (j < 0)
+                    {
+                        positions.Add(s);
+                        s += goodSuffixShift[0];
+                    }
+                    else
+                    {
+                        int badCharShift = Math.Max(1, j - badCharTable.GetValueOrDefault(text[s + j], -1));
+                        s += Math.Max(badCharShift, goodSuffixShift[j + 1]);
+                    }
+                }
+                return positions;
+            }
+
+            public static void PreprocessStrongSuffix(int[] shift, int[] bpos,
+                                                     string pat, int m)
+            {
+                int i = m, j = m + 1;
+                bpos[i] = j;
+
+                while (i > 0)
+                {
+                    
+                    while (j <= m && pat[i - 1] != pat[j - 1])
+                    {
+                        if (shift[j] == 0)
+                            shift[j] = j - i;
+
+                        j = bpos[j];
+                    }
+                    i--; j--;
+                    bpos[i] = j;
+                }
+            }
+
+            public static void PreprocessCase2(int[] shift, int[] bpos,
+                                              string pat, int m)
+            {
+                int j = bpos[0];
+                for (int i = 0; i <= m; i++)
+                {
+                    if (shift[i] == 0)
+                        shift[i] = j;
+
+                    if (i == j)
+                        j = bpos[j];
+                }
+            }
+
+            private static Dictionary<char, int> BuildBadCharTable(string pattern)
+            {
+                var table = new Dictionary<char, int>();
+                for (int i = 0; i < pattern.Length - 1; i++)
+                    table[pattern[i]] = i;
+                return table;
+            }
+        }
+    }
+
+/*
+ m - длина строки
+ n - длинна паттерна
+ В худшем случае О(m * n)
+ В лучшем (m / n)
+ Средний случай близок к лучшему.
+ Многое зависит от количества уникальных символов как в алфавите(строке), так и в паттерне:
+ чем больше уникальных символов в паттерне - тем меньше операций сравнения понадобиться,
+ также чем больше уникальных символов в алфавите(строке) - тем меньше операций сравнения понадобиться.
+ */
---
+++ BoierMourAlghorithm/BoierMourAlghorithm/Program.cs
@@ -0,0 +1,62 @@
+﻿using BierMourAlghorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BoierMourAlghorithm
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] filePaths = Directory
+                .GetFiles(
+                "C:\\Users\\Dima\\source\\repos\\BoierMourAlghorithm\\BoierMourAlghorithm\\TestFiles"
+                );
+
+            foreach (var filePath in filePaths)
+            {
+                Tester.TestBoyerMoore(
+                    File.ReadAllText(filePath),
+                    Path.GetFileNameWithoutExtension(filePath).Split("_").Last()
+                    );
+            }
+        }
+
+        public static int CountUniqueCharacters(string text)
+        {
+            HashSet<char> uniqueChars = new HashSet<char>();
+
+            foreach (char c in text)
+            {
+                if (!Char.IsWhiteSpace(c)) // если нужно игнорировать пробелы
+                    uniqueChars.Add(c);
+            }
+
+            return uniqueChars.Count;
+        }
+    }
+    public class Tester
+    {
+        public static void TestBoyerMoore(string text, string pattern)
+        {
+            Console.WriteLine($"\nШаблон: {pattern}");
+            Console.WriteLine($"Длина паттерна {pattern.Length}");
+            Console.WriteLine($"Количество уникальных символов алфавита {Program.CountUniqueCharacters(text)}");
+            Console.WriteLine($"Длина строки: {text.Length}");
+            var searcher = new BoyerMoore();
+            var result = searcher.Search(text, pattern);
+
+            Console.WriteLine($"Индекс последнего вхождения: {result.Last().ToString()}");
+            if (result.Count == 0)
+            {
+                Console.WriteLine("Вхождений не найдено.");
+            }
+            else
+                Console.WriteLine($"Найдено вхождений {result.Count}");
+
+            Console.WriteLine($"Количество операций сравнения: {searcher.OperationsCount}");
+        }
+    }
+}
